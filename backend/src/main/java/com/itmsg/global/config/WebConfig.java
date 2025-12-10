3ab60735e0b36407870adb1fb1c76eed0bf7897c
Symphonia <intel72440bx@gamil.com>
2025-12-10 21:54:02 +0900

    소스 경로 변경

--- backend/src/main/java/com/itmsg/global/config/WebConfig.java
+++ backend/src/main/java/com/itmsg/global/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("http://localhost:3009", "http://localhost:8089")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
