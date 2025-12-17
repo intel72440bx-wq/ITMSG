7bd4493051cf7c7efc0fd06a3d7a302063ddffe2
Symphonia <intel72440bx@gamil.com>
2025-12-17 20:22:55 +0900

    환경정보 추가

--- backend/src/main/java/com/itmsg/global/config/WebConfig.java
+++ backend/src/main/java/com/itmsg/global/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3009", "http://localhost:8089")
+                .allowedOrigins("http://121.164.146.192:3009", "http://121.164.146.192:8089")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
